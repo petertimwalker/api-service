f573572a27b67b3630b04c45d2f2728337f78415
Pete Walker <108812366+petertimwalker@users.noreply.github.com>
2024-05-24 12:18:27 -0400

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Use CORS middleware to allow requests from all origins
+app.use(cors());
 
 // Store your API key securely in an environment variable
 const API_KEY = process.env.API_KEY || 'API_KEY not defined';
