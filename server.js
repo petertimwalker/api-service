3d219b5305b48459bf3b0de14da20b50bfac9eee
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2024-05-24 20:24:49 +0000

    add logging on requests

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.use(cors());
 // Store your API key securely in an environment variable
 const API_KEY = process.env.API_KEY || 'API_KEY not defined';
 
+// Middleware to log requests to the terminal
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  console.log(`request from ${req}`);
+  res.send('Hi from api.peterwalker.xyz');
+});
+
 app.get('/api/key', (req, res) => {
   console.log(`request from ${req}`);
   res.json({ apiKey: API_KEY });
