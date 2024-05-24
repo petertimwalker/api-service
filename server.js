74d6a39ef041b1a82a28fd1ea74ff45459ec78be
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2024-05-24 20:53:21 +0000

    add dotenv to load envs

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 443;
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Use CORS middleware to allow requests from all origins
 app.use(cors());
